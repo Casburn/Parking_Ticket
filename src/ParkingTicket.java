75740d78a3f43c9aa874c774453c59861268d62d
Casburn <robert.casburn@atos.net>
2015-10-20 15:59:32 +0100

    editing code

--- src/ParkingTicket.java
+++ src/ParkingTicket.java
@@ -1,5 +1,25 @@
-
 public class ParkingTicket
 {
+    public String regNum;
 
+    public int hours, minutes;
+
+    public boolean prepaid;
+
+    public int leavingTime, arrivalTime;
+
+    public void validTicket()
+    {
+        this.prepaid = false;
+    }
+
+    public void timeRemaining()
+    {
+        // to do
+    }
+
+    public void arrivalTime()
+    {
+        this.arrivalTime = 1200;
+    }
 }
--- src/ParkingTransaction.java
+++ src/ParkingTransaction.java
@@ -1,5 +1,4 @@
-
 public interface ParkingTransaction
 {
-
+    public void ParkingTicket1();
 }
