c239cf0051f5077494599d08882dc35719e9b05a
Casburn <robert.casburn@atos.net>
2015-10-22 11:30:27 +0100

    Added some code

--- src/ParkingTicket.java
+++ src/ParkingTicket.java
@@ -23,11 +23,19 @@ public class ParkingTicket
         for (int i = 0; i < 1; i++)
         {
             int num = random.nextInt(900) + 100;
-
-            num = random.nextInt(900) + 100;
             System.out.println(" Transaction Number: " + num);
         }
 
+    }
+
+    public void timeArrived()
+    {
+        Random rand = new Random();
+
+        for (int i = 0; i < 1; i++)
+        {
+            int hour = rand.nextInt(24) + 0;
+        }
     }
 
     public void timeRemaining()
