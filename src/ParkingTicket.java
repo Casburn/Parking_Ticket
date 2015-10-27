d590bead9d3b04774d5374a855d31ae41b9cd44a
Casburn <robert.casburn@atos.net>
2015-10-27 09:44:24 +0000

    uncoding the use of the text file

--- src/ParkingTicket.java
+++ src/ParkingTicket.java
@@ -1,44 +1,10 @@
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.IOException;
 import java.util.Random;
 
 public class ParkingTicket
 {
-    public static void main(String[] args) throws IOException
+    public static void main(String[] args)
     {
         System.out.println("Transaction: " + transactionNum());
-
-        Ticket theTicket = new Ticket();
-        BufferedReader in = new BufferedReader(new FileReader("ParkingTickets.txt"));
-        String line;
-        while ((line = in.readLine()) != null)
-        {
-            for (int i = 0; i < 1; ++i)
-                in.readLine();
-            String lineIWant = in.readLine();
-            System.out.println(lineIWant);
-        }
-
-        String info;
-
-        int startLine = 1;
-
-        int endLine = 2;
-
-        for (int i = 0; i < startLine; i++)
-        {
-            info = in.readLine();
-        }
-
-        for (int i = startLine; i < endLine + 1; i++)
-        {
-
-            info = in.readLine();
-
-            System.out.println(info);
-        }
-        in.close();
     }
 
     private static int transactionNum()
--- src/Ticket.java
+++ src/Ticket.java
@@ -1,71 +1,24 @@
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.IOException;
-import java.util.Scanner;
-
-class Tickets
-{
-    String regNum;
-
-    String latestLeavingTime;
-
-    String arrivalTime;
-
-    String prepaid;
-}
-
 public class Ticket
 {
-
-    public void main(String[] args) throws IOException
+    public void main(String[] args)
     {
-        TheTicket();
+        ParkingTicketOne();
+        String regNum1 = "SY65 OED";
+        int arrivalTime1 = 1406;
+        boolean prepaid1 = false;
+        int latestLeavingTime1 = 1606;
+
+        System.out.println(regNum1 + arrivalTime1 + prepaid1 + latestLeavingTime1);
     }
 
-    public void TheTicket() throws IOException
+    public void ParkingTicketOne()
     {
-        Scanner fin = new Scanner(new FileReader("ParkingTickets" + ".txt"));
-        BufferedReader in = new BufferedReader(new FileReader("ParkingTickets.txt"));
-        String line;
-        while ((line = in.readLine()) != null)
-        {
-            System.out.println(line);
-        }
-        in.close();
+        String regNum1 = "SY65 OED";
+        int arrivalTime1 = 1406;
+        boolean prepaid1 = false;
+        int latestLeavingTime1 = 1606;
 
-        Tickets one = new Tickets();
-        one.regNum = fin.nextLine();
-        one.arrivalTime = fin.nextLine();
-        one.prepaid = fin.nextLine();
-        one.latestLeavingTime = fin.nextLine();
+        System.out.println(regNum1 + arrivalTime1 + prepaid1 + latestLeavingTime1);
 
-        Tickets two = new Tickets();
-        two.regNum = fin.nextLine();
-        two.arrivalTime = fin.nextLine();
-        two.prepaid = fin.nextLine();
-        two.latestLeavingTime = fin.nextLine();
-
-        Tickets three = new Tickets();
-        three.regNum = fin.nextLine();
-        three.arrivalTime = fin.nextLine();
-        three.prepaid = fin.nextLine();
-        three.latestLeavingTime = fin.nextLine();
-
-        Tickets four = new Tickets();
-        four.regNum = fin.nextLine();
-        four.arrivalTime = fin.nextLine();
-        four.prepaid = fin.nextLine();
-        four.latestLeavingTime = fin.nextLine();
-
-        fin.close();
-
-        System.out.println(" PARKING TICKET\n+--------------+\n " + one.regNum + "\n " + one.arrivalTime + "\n "
-                + one.prepaid + " \n " + one.latestLeavingTime + "\n");
-        System.out.println(" PARKING TICKET\n+--------------+\n " + two.regNum + "\n " + two.arrivalTime + "\n "
-                + two.prepaid + " \n " + two.latestLeavingTime + "\n");
-        System.out.println(" PARKING TICKET\n+--------------+\n " + three.regNum + "\n " + three.arrivalTime + "\n "
-                + three.prepaid + " \n " + three.latestLeavingTime + "\n");
-        System.out.println(" PARKING TICKET\n+--------------+\n " + four.regNum + "\n " + four.arrivalTime + "\n "
-                + four.prepaid + " \n " + four.latestLeavingTime);
     }
 }
