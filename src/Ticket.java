af39b05c718503c0a251043fde667d490c26b566
Casburn <robert.casburn@atos.net>
2015-10-29 13:52:53 +0000

    got the price to print out correctly

--- src/Ticket.java
+++ src/Ticket.java
@@ -48,15 +48,51 @@ public class Ticket
         regNum = "SW02 DVA";
         arrivalTime = 20.35;
         prepaid = true;
-        lengthOfTime = 7.41;
 
         return (regNum);
     }
 
-    public int driveInLeaveTime()
+    public double driveInLeaveTime()
     {
         Random ran = new Random();
-        int number = ran.nextInt(24);
+        double number = ran.nextInt(24);
+
+        double cost = 0;
+        if (number <= 1)
+        {
+            cost = 4.70;
+            System.out.println("  Cost: £" + cost);
+        }
+        else if (number > 1 && number <= 2)
+        {
+            cost = 7.40;
+            System.out.println("  Cost: £" + cost);
+        }
+        else if (number >= 3 && number <= 4)
+        {
+            cost = 10.30;
+            System.out.println("  Cost: £");
+        }
+        else if (number >= 5 && number <= 6)
+        {
+            cost = 14.80;
+            System.out.println("  Cost: £" + cost);
+        }
+        else if (number >= 7 && number <= 9)
+        {
+            cost = 17.80;
+            System.out.println("  Cost: £" + cost);
+        }
+        else if (number >= 10 && number <= 12)
+        {
+            cost = 20.20;
+            System.out.println("  Cost: £" + cost);
+        }
+        else if (number >= 13 && number <= 24)
+        {
+            cost = 23.70;
+            System.out.println("  Cost: £" + cost);
+        }
 
         return number;
     }
