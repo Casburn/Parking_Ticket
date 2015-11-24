979f78ad8ea979ee83df641537d893d93f33e4b0
Casburn <robert.casburn@atos.net>
2015-11-24 16:29:23 +0000

    set reminders of text on what to do

--- src/WeekdayCharge.java
+++ src/WeekdayCharge.java
@@ -4,8 +4,16 @@ public class WeekdayCharge
 {
     // Manages weekday charges and returns a charge based on the length of stay
 
+    // Rename method, make a new if with costs nested within it, else the weekend charge, if needs to find what day in
+    // the week it is.
     public double driveInLeaveTime(Ticket tickets, Date timeNow)
     {
+        /*
+         * Calendar c = Calendar.getInstance();
+         * c.setTime(yourDate);
+         * int dayOfWeek = c.get(Calendar.DAY_OF_WEEK);
+         */
+
         // Calls calculation for time stayed and checks for what the cost will be and returns
         long diffHours = tickets.differentHours(timeNow);
         double cost = 0;
@@ -46,23 +54,23 @@ public class WeekdayCharge
     public int weekendCharge(Ticket tickets, Date timeNow)
     {
         long diffHours = tickets.differentHours(timeNow);
-        double cost1 = 0;
+        double cost = 0;
 
         if (diffHours > 0 && diffHours <= 1)
         {
-            cost1 = 8.60;
-            System.out.println("  Cost: £" + cost1);
+            cost = 8.60;
+            System.out.println("  Cost: £" + cost);
         }
         else if (diffHours > 1 && diffHours <= 2)
         {
-            cost1 = 10.80;
-            System.out.println("  Cost: £" + cost1);
+            cost = 10.80;
+            System.out.println("  Cost: £" + cost);
         }
         else
         {
-            cost1 = 25.40;
-            System.out.println("  Cost: £" + cost1);
+            cost = 25.40;
+            System.out.println("  Cost: £" + cost);
         }
-        return cost1;
+        return cost;
     }
 }
--- src/WeekendCharge.java
+++ src/WeekendCharge.java
@@ -4,33 +4,7 @@ public class WeekendCharge(Ticket tickets, Date timeNow)
 {
     // managaes the weekend charges and returns a charge based on the length of stay
     
-    double[] cost = new double[24];
-    {
-        cost[0] = 0;
-        cost[1] = 4.70;
-        cost[2] = 7.40;
-        cost[3] = 10.30;
-        cost[4] = 10.30;
-        cost[5] = 14.80;
-        cost[6] = 14.80;
-        cost[7] = 17.80;
-        cost[8] = 17.80;
-        cost[9] = 17.80;
-        cost[10] = 20.20;
-        cost[11] = 20.20;
-        cost[12] = 20.20;
-        cost[13] = 23.70;
-        cost[14] = 23.70;
-        cost[15] = 23.70;
-        cost[16] = 23.70;
-        cost[18] = 23.70;
-        cost[19] = 23.70;
-        cost[20] = 23.70;
-        cost[21] = 23.70;
-        cost[23] = 23.70;
-        cost[24] = 23.70;
-    }
-    
+   
     long diffHours = tickets.differentHours(timeNow);
     double cost1 = 0;
 
