215ce9fc219696931059fc289e2b3e6ced8f767f
Casburn <robert.casburn@atos.net>
2015-11-30 10:27:46 +0000

    Drive in ticket

--- src/DriveInTicket.java
+++ src/DriveInTicket.java
@@ -1,5 +1,18 @@
-public class DriveInTicket extends ParkingTicketValidator
+import java.util.Date;
+
+public class DriveInTicket extends ParkingTicketFactory
 {
+
+    public DriveInTicket(String regNum, Date arrivalTime, boolean prepaid)
+    {
+        super(regNum, arrivalTime, prepaid);
+    }
+
+    public DriveInTicket(String regNum, Date arrivalTime, boolean prepaid, Date latestLeavingTime)
+    {
+        super(regNum, arrivalTime, prepaid, latestLeavingTime);
+    }
     // Sets ticket type to drive in
     // Gets details from parking ticket validator
+
 }
